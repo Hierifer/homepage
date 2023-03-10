3400e9e9f3b0e46fb3cba0ebc0734b280355da10
Hierifer <hierifer@hotmail.com>
2023-03-10 14:50:01 +0800

    fix: animation

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -19,8 +19,13 @@ const Home = () => {
   const t1Ref = useRef<HTMLDivElement>(null);
   const t2Ref = useRef<HTMLDivElement>(null);
   const t3Ref = useRef<HTMLDivElement>(null);
+  const productRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const joinRef = useRef<HTMLDivElement>(null);
 
+  // 视差动画存在视野
   const [scrollAnimating, setScrollAnimating] = useState(false);
+  const [activeTab, setActiveTab] = useState('');
 
   let foreHeight = 50;
 
@@ -32,41 +37,57 @@ const Home = () => {
     let ah2 = 0
     let ah3 = 0
     ah1 = (h * foreHeight / 100) !== 0? h * foreHeight / 100 : 100 // stop animate
-    ah2 = 2 * ah1
-    ah3 = 3* ah1
-    console.log(value)
 
+    // 控制视差动画
     if(value < ah1) {
       let step = value / ah1
-      foreRef.current!.style.transform = `translate(-50%, calc(100% - ${value}px ))`
       wholeRef.current!.style.position = 'fixed'
       t1Ref.current!.style.opacity = `${1 - step}`
+      t2Ref.current!.style.opacity = '0'
+      t3Ref.current!.style.opacity = '0'
+      foreRef.current!.style.transform = `translate(-50%, 100%)`
+    } else if(value < 2 * ah1 && value >= ah1) {
+      let step = (value - ah1) / ah1
+      wholeRef.current!.style.position = 'fixed'
+      t1Ref.current!.style.opacity = '0'
       t2Ref.current!.style.opacity = `${step}`
       t3Ref.current!.style.opacity = '0'
-      //holdRef.current.style.display="block"
-      setScrollAnimating(true);
-    } else if(value < ah2 && value >= ah1) {
-      wholeRef.current!.style.position = 'fixed'
+      foreRef.current!.style.transform = `translate(-50%, calc(100% - ${value - ah1}px ))`
+    } else if(value < 3 * ah1 && value >= 2 * ah1) {
+      let step = (value - ah1 * 2) / ah1
       t1Ref.current!.style.opacity = '0'
-      t2Ref.current!.style.opacity = `${1 - (value - ah1) / ah1}`
-      t3Ref.current!.style.opacity = `${(value - ah1) / ah1}`
-      setScrollAnimating(true);
-    } else if(value < ah3 && value >= ah2) {
+      t2Ref.current!.style.opacity = `${1 - step}`
+      t3Ref.current!.style.opacity = '0'
+      wholeRef.current!.style.position = 'fixed'
+    } else if(value < 4 * ah1 && value >= 3 * ah1) {
+      let step = (value - ah1 * 3) / ah1
       t1Ref.current!.style.opacity = '0'
       t2Ref.current!.style.opacity = '0'
+      t3Ref.current!.style.opacity = `${step}`
       wholeRef.current!.style.position = 'fixed'
-      setScrollAnimating(true);
-    } else if(value < 2.5 * h && value >= ah3) {
-      wholeRef.current!.style.position = 'absolute'
-      setScrollAnimating(true);
-      //holdRef.current.style.display="hidden"
+    } else if(value < 5 * ah1 && value >= 4 * ah1){
+
+      wholeRef.current!.style.position = 'fixed'
     } else {
+      t3Ref.current!.style.transform = `translate(-50%, calc(-50% - ${(value - ah1 * 5)/2}px ))`
+      wholeRef.current!.style.position = 'absolute'
+    } 
+
+    // 控制 Header 展示
+    if(value < 3.5 * h){
+      // Header 使用浅色
+      document.getElementsByTagName('body')[0].style.setProperty('--gray-10', '229,230,235')
+      setScrollAnimating(true);
+    } else {
+      // Header 使用深色
+      document.getElementsByTagName('body')[0].style.setProperty('--gray-10', '39,46,59')
       setScrollAnimating(false);
     }
   }
 
   useEffect(() => {
     if(!init){
+      storybook()
       window.addEventListener('scroll', function(){
         storybook()
       })
@@ -77,39 +98,39 @@ const Home = () => {
   return (
     <React.Fragment>
       <Header dark={scrollAnimating} />
-        <div ref={holdRef} className="relative" style={{width: '100%', height: '250vh'}}>
+        <div ref={holdRef} className="relative" style={{width: '100%', height: '350vh'}}>
           <div ref={wholeRef} className="w-screen h-screen flex justify-center overflow-hidden bottom-0">
             <div className="absolute inset-1/2 w-full text-white" style={{zIndex: 20, transform: 'translate(-50%, -40%)', textAlign: 'center'}}>
-              <p ref={t1Ref} className="text-5xl opacity-100">人生如繁星，精彩却孤独</p>
-              <p ref={t2Ref} className="text-5xl opacity-0">我们，观星者们</p>
-              <p ref={t3Ref} className="text-5xl opacity-0">将发现他人与你的连接</p>
+              <p ref={t1Ref} className="absolute left-1/2 text-5xl opacity-100" style={{transform: 'translate(-50%, -50%)'}}>人生如繁星，精彩却孤独</p>
+              <p ref={t2Ref} className="absolute left-1/2 text-5xl opacity-0" style={{transform: 'translate(-50%, -50%)'}}>我们，观星者们</p>
+              <p ref={t3Ref} className="absolute left-1/2 text-5xl opacity-0" style={{transform: 'translate(-50%, -50%)'}}>将发现他人与你的连接</p>
             </div>
             <img src={s1} ref={backRef} style={{width:'100%', height: '100%',transform: 'translateY(0%)'}} className="object-cover absolute" alt="logo" />
             <img src={s2} ref={foreRef} style={{height: `${foreHeight}%`, left: '50%', transform: 'translate(-50%, 100%)'}} className="z-30 object-cover absolute bottom-0" alt="logo" />
             <img src={scrollDown} className="absolute bottom-3 left-1/2 w-8 z-40" style={{transform: 'translate(-50%, -50%)'}} />
           </div>
         </div>
-        <div className="w-screen h-screen flex justify-between">
-          <div style={{width: '50%'}} className="h-full flex items-center justify-center">
-            <div style={{width: '100%'}}  className="relative flex justify-center">
-              <div style={{width:'45%', transform: 'translate(-20%, 20%)', aspectRatio: '1', background: "#4141c9", borderRadius: "10%"}} className="absolute"></div>
-              <img src={letjoy} style={{width:'50%'}} className="object-cover z-20" alt="logo" />
-            </div>
-          </div>
-          <Element name="product">
+        <Element name="product">
+          <div ref={productRef} className="w-screen h-screen flex justify-between">
             <div style={{width: '50%'}} className="h-full flex items-center justify-center">
-              <div style={{width: '100%'}}  className="relative flex-col justify-center">
-                <p className="text-5xl pb-5" >来聚组局</p>
-                <p className="text-xl">通过算法组局，精准匹配你的线下剧本杀玩伴</p>
-                <p><a href="/letjoy" className="text-xl" style={{color: 'rgb(var(--blue-7))'}}>了解更多</a></p>
+              <div style={{width: '100%'}}  className="relative flex justify-center">
+                <div style={{width:'45%', transform: 'translate(-20%, 20%)', aspectRatio: '1', background: "#4141c9", borderRadius: "10%"}} className="absolute"></div>
+                <img src={letjoy} style={{width:'50%'}} className="object-cover z-20" alt="logo" />
               </div>
             </div>
-          </Element>
-
-        </div>
+            
+              <div style={{width: '50%'}} className="h-full flex items-center justify-center">
+                <div style={{width: '100%'}}  className="relative flex-col justify-center">
+                  <p className="text-5xl pb-5" >来聚组局</p>
+                  <p className="text-xl">通过算法组局，精准匹配你的线下剧本杀玩伴</p>
+                  <p><a href="/letjoy" className="text-xl" style={{color: 'rgb(var(--blue-7))'}}>了解更多</a></p>
+                </div>
+              </div>
+          </div>
+        </Element>
 
         <Element name="about">
-          <div className="w-screen h-screen flex justify-center">
+          <div ref={aboutRef} className="w-screen h-screen flex justify-center">
             <div className="max-w-6xl w-full flex flex-col justify-center items-center p-5">
               <div>
                 <h1>关于我们</h1>
@@ -125,7 +146,7 @@ const Home = () => {
         </Element>
 
         <Element name="join">
-          <div className="w-screen h-screen flex justify-center">
+          <div ref={joinRef} className="w-screen h-screen flex justify-center">
             <div className="max-w-6xl w-full flex flex-col justify-center items-center p-5">
               <div className="w-full relative mb-10">
                 <h1>加入我们</h1>
@@ -151,7 +172,6 @@ const Home = () => {
             </div>
           </div>
         </Element>
-
       <Footer />
     </React.Fragment>
   );
