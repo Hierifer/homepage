9fad0f92cd85f2b9ca25c6550c51f03fd9b38edd
Hierifer <hierifer@hotmail.com>
2023-05-21 16:05:40 +0800

    feat: [position] add sticky

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,24 +8,36 @@ import scrollDown from '../static/animation/scroll-down-w.gif'
 import hiring from '../static/picture/hiring.png'
 import qr from '../static/picture/letjoy-qr.jpg'
 import './Home.css'
-import * as THREE from 'three';
+import { Tabs, Timeline } from '@arco-design/web-react';
+const TabPane = Tabs.TabPane;
+const TimelineItem = Timeline.Item;
+
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 const langPack:{[key: string]: {[key:string]: string}} = {
   'st1' : {'cn': '人生如同繁星，精彩却又孤独', 'en': 'Life is great but transient'},
-  'st2' : {'cn': '我们，观星者们', 'en': 'We, Soical Astronomer'},
+  'st2' : {'cn': '克莱克特', 'en': 'Collect'},
   'st3' : {'cn': '发现你与他人更多的连接', 'en': 'find more connect between you and others'},
   'letjoy' : {'cn': '来聚组局', 'en': 'Letjoy'},
   'ljdesc' : {'cn': '通过算法组局，精准匹配你的线下剧本杀玩伴', 'en': 'Match your boardgame player effectively with big-data'},
   'about' : {'cn': '关于我们', 'en': 'About us'},
-  'about1' : {'cn': '克莱克特是一家具有算法基因的互联网公司，我们致力于应用数据和算法去发掘用户们潜在的社交需求，催化人与人之间化学键并释放其中的能量与价值。2023 年，我们上线第一款社交型产品「来聚组局」，其以前置需求匹配的交互模式给用户提供了全新的社交体验。', 
+  'about1' : {'cn': '克莱克特是一家具有算法基因的互联网公司。我们致力于应用数据和算法去发掘用户们潜在的社交需求，催化人与人之间化学键并释放其中的能量与价值。2023 年，我们上线第一款社交型产品「来聚组局」，其以前置需求匹配的交互模式给用户提供了全新的社交体验。', 
               'en': 'Connect is an \'Algorithm-based\' tech corp, which concentrates on exploring and mining the social relationship and communication with big-data and algorithm. In 2023, the first product, aka \'Letjoy\' is launched at WeChat miniprogram, that brings a new experience to the customers.'},
-  'about2' : {'cn': '观星者（克莱克特人）的预言是「人的未来不应该是孤独的，任何人都有社交的需求。他们只需要更多的相遇」。而我们的魔法（算法）可以他们建立更多精准的连接，制造更多妙不可言的相遇。', 
-              'en': 'Astronomers\' (Connecters) vision is \'Everyone\'s future is not alone as everyone needs social in other life. And, they need more chance to meet others\'. And our magic is to create the great chance for them'},             
+  'about2' : {'cn': '克莱克特认为「任何人都有社交的需求，但这大多数人都生活在中心化，严重熵增的信息茧房里。失去了社交的信息基础」。而我们的愿景是帮组所有人建立更多的连接，去认识更多的人，看到更多不一样的个体。', 
+              'en': 'Collect\'s vision is \'Everyone\'s future is not alone as everyone needs social in other life. And, they need more chance to meet others\'. And our app is to create the great chance for them'},             
   'career' : {'cn': '加入我们', 'en': 'Career'},
-  'careerdesc' : {'cn': '星辰正在启示新的可能，你愿意加入我们吗？新的观星者', 'en': 'Stars reveal the new future. Will you join us? The new Astronomer'},
+  'perspective': {'cn': '愿景和使命', 'en': 'Perspectives'},
+  'values': {'cn': '价值观', 'en': 'Values'},
+  'value1': {'cn': '专注目标：专业的人做专业的事情，重视目标而非途径', 'en': 'Being professnial and Focus the goal: Doing and done with the professional attitude and skills. Focus on the goal instead of approaches'},
+  'value2': {'cn': '平等坦诚：大家都有合理建议和批评的权利与义务', 'en': 'Be candid and keep critical: Everyone has rights to suggest and crticize in reason'},
+  'value3': {'cn': '创新敢为：如果有想法，请立刻喊出出来', 'en': 'Creativity and brave to do: Yarn out once has an genius idea'},
+  'value4': {'cn': '耐心聆听：把身段放低，以学生和朋友的身份去聆听和学习', 'en': 'Listen and be patient: Listen and learn from the other in the role of students and friends'},
+  'stories': {'cn': '我们的故事', 'en': 'Stories'},
+  'story1': {'cn': '上海克莱克特创立','en': 'Collect is registered'},
+  'story2': {'cn': '核心团队招募完毕', 'en': 'The core team is collected'},
+  'story3': {'cn': '来聚组局上线微信小程序', 'en': 'Letjoy is debut on WeChat Miniprogram'},
   'position' : {'cn': '工作岗位', 'en': 'Position'},
-  'devPositionTitle' : {'cn': '研发观星师', 'en': 'Engineer Aston'},
+  'devPositionTitle' : {'cn': '前端工程师', 'en': 'Engineer Aston'},
   'devPositionDesc' : {'cn': '描述描述', 'en': 'Desc Desc'},
 }
 
@@ -57,43 +69,6 @@ const Home = () => {
   let foreHeight = 50;
   let ticking = false;
 
-  const initThree = () => {
-    // const scene = new THREE.Scene(); 
-    // const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
-    // const renderer = new THREE.WebGLRenderer({ antialias: true }); 
-    // renderer.setSize( window.innerWidth, window.innerHeight ); 
-    // document.getElementsByClassName("3back")[0].appendChild( renderer.domElement ); 
-
-    // const cbg = new THREE.BoxGeometry( 1, 1, 1 ); 
-    // const cbm = new THREE.MeshLambertMaterial( { color: 'blue' } ); 
-    
-    // const cng = new THREE.ConeGeometry( .5, 1, 32 );
-    // const cnm = new THREE.MeshLambertMaterial( { color: 'red' } ); 
-    // const light = new THREE.AmbientLight(0xFFFFFF, .4);
-    // const dlight = new THREE.DirectionalLight(0xFFFFFF, .5);
-    // dlight.position.set(10, 10, 0);
-    // dlight.target.position.set(-5, 0, 0);
-    // scene.add(light);
-    // scene.add(dlight);
-    // const cube = new THREE.Mesh( cbg, cbm );
-    // cube.position.set(-1, 1, 0);
-    // const cone = new THREE.Mesh(cng, cnm); 
-    // cone.position.set(1, 1, 0);
-    // cone.rotation.z = 0.1;
-    // scene.add( cube ); 
-    // scene.add( cone );
-    // camera.position.z = 5; 
-    // renderer.setClearColor( 0xffffff )
-    // function animate() { 
-    //   requestAnimationFrame( animate ); 
-    //   cube.rotation.x += 0.001;
-    //   cube.rotation.z += 0.001; 
-    //   cone.rotation.y += 0.01;
-    //   renderer.render( scene, camera ); 
-    // } 
-    // animate();
-  }
-
   const storybook = () => {
     let value = window.scrollY;
     let h = window.innerHeight;
@@ -111,46 +86,39 @@ const Home = () => {
           t2Ref.current!.style.opacity = '0'
           t3Ref.current!.style.opacity = '0'
           foreRef.current!.style.transform = `translate(-50%, 100%)`
-          wholeRef.current!.style.transform = `translateY(0)`
         } else if(value < 2 * ah1 && value >= ah1) {
           let step = (value - ah1) / ah1
           t1Ref.current!.style.opacity = '0'
           t2Ref.current!.style.opacity = `${step}`
           t3Ref.current!.style.opacity = '0'
           foreRef.current!.style.transform = `translate(-50%, calc(100% - ${value - ah1}px ))`
-          wholeRef.current!.style.transform = `translateY(0)`
         } else if(value < 3 * ah1 && value >= 2 * ah1) {
           let step = (value - ah1 * 2) / ah1
           t1Ref.current!.style.opacity = '0'
           t2Ref.current!.style.opacity = `${1 - step}`
           t3Ref.current!.style.opacity = '0'
           foreRef.current!.style.transform = `translate(-50%,  0)`
-          wholeRef.current!.style.transform = `translateY(0)`
         } else if(value < 4 * ah1 && value >= 3 * ah1) {
           let step = (value - ah1 * 3) / ah1
           t1Ref.current!.style.opacity = '0'
           t2Ref.current!.style.opacity = '0'
           t3Ref.current!.style.opacity = `${step}`
           foreRef.current!.style.transform = `translate(-50%,  0)`
-          wholeRef.current!.style.transform = `translateY(0)`
         } else if(value < 5 * ah1 && value >= 4 * ah1){
           t1Ref.current!.style.opacity = '0'
           t2Ref.current!.style.opacity = '0'
           t3Ref.current!.style.opacity = `1`
           foreRef.current!.style.transform = `translate(-50%,  0)`
-          wholeRef.current!.style.transform = `translateY(0)`
         } else if(value < 7 * ah1 && value >= 5 * ah1){
           t1Ref.current!.style.opacity = '0'
           t2Ref.current!.style.opacity = '0'
           t3Ref.current!.style.opacity = `1`
           foreRef.current!.style.transform = `translate(-50%,  0)`
-          wholeRef.current!.style.transform = `translateY(${ah1*5-value}px)`
         } else {
           t1Ref.current!.style.opacity = '0'
           t2Ref.current!.style.opacity = '0'
           t3Ref.current!.style.opacity = `1`
           foreRef.current!.style.transform = `translate(-50%,  0)`
-          wholeRef.current!.style.transform = `translateY(-100%)`
         }
 
         // 控制 Header 展示
@@ -196,7 +164,6 @@ const Home = () => {
     if(!init){
       console.log(init)
       storybook()
-      initThree()
       window.addEventListener('scroll', function(){
         storybook()
       })
@@ -211,7 +178,7 @@ const Home = () => {
     <React.Fragment>
         <div ref={holdRef} className="relative overflow-hideen" style={{width: '100%', height: '350vh'}}>
           <Header dark={scrollAnimating} lang={lang} setLang={setLang} activeTab={activeTab} setActiveTab={setActiveTab}/>
-          <div ref={wholeRef} className="w-screen h-screen flex justify-center overflow-hidden bottom-0 fixed whole">
+          <div ref={wholeRef} className="w-screen h-screen flex justify-center overflow-hidden bottom-0 top-0 sticky whole">
             <div className="absolute w-full text-white" style={{zIndex: 20, top: '40%', left: '50%', transform: 'translate(-50%, -40%)', textAlign: 'center'}}>
               <p ref={t1Ref} className="absolute left-1/2 text-5xl opacity-100" style={{transform: 'translate(-50%, -50%)'}}>{langPack.st1[lang]}</p>
               <p ref={t2Ref} className="absolute left-1/2 text-5xl opacity-0" style={{transform: 'translate(-50%, -50%)'}}>{langPack.st2[lang]}</p>
@@ -250,15 +217,43 @@ const Home = () => {
           <div ref={aboutRef} className={`w-screen h-screen flex justify-center moveable ${aMove? 'translate-y-0':'translate-y-1/4'}`}>
             <div className="max-w-6xl w-full flex flex-col justify-center items-center p-5">
               <div className="pb-10">
-                <p className="text-3xl my-5">{langPack.about[lang]}</p>
-                <p className="text-xl pb-5" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
-                  {langPack.about1[lang]}
-                </p>
-                <p className="text-xl" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
-                  {langPack.about2[lang]}
-                </p>
+                <p className="text-3xl my-10">{langPack.about[lang]}</p>
+                <Tabs tabPosition='left' scrollPosition="center" type="rounded" size="large" style={{'height': '60%', 'minHeight': '400px'}}>
+                  <TabPane key='tab1' title={langPack.perspective[lang]}>
+                    <p className="text-2xl mb-5">{langPack.perspective[lang]}</p>
+                    <p className="text-xl pb-5" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
+                      {langPack.about1[lang]}
+                    </p>
+                    <p className="text-xl" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
+                      {langPack.about2[lang]}
+                    </p>
+                  </TabPane>
+                  <TabPane key='tab2' title={langPack.values[lang]}>
+                    <p className="text-2xl mb-5">{langPack.values[lang]}</p>
+                    <p className="text-xl pb-5" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
+                      {langPack.value1[lang]}
+                    </p>
+                    <p className="text-xl pb-5" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
+                      {langPack.value2[lang]}
+                    </p>
+                    <p className="text-xl pb-5" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
+                      {langPack.value3[lang]}
+                    </p>
+                    <p className="text-xl pb-5" style={{lineHeight: '2.5rem', color: 'rgb(var(--gray-7))'}}>
+                      {langPack.value4[lang]}
+                    </p>
+                  </TabPane>
+                  <TabPane key='tab3' title={langPack.stories[lang]}>
+                    <Timeline reverse={true}>
+                      <TimelineItem label='2022-10-10'><p className="text-xl">{langPack.story1[lang]}</p></TimelineItem>
+                      <TimelineItem label='2022-11-30'><p className="text-xl">{langPack.story2[lang]}</p></TimelineItem>
+                      <TimelineItem label='2023-01-01'><p className="text-xl">{langPack.story3[lang]}</p></TimelineItem>
+                    </Timeline>
+                  </TabPane>
+                </Tabs>
               </div>
             </div>
+
           </div>
         </Element>
 
@@ -267,9 +262,6 @@ const Home = () => {
             <div className="max-w-6xl w-full flex flex-col justify-center items-center p-5">
               <div className="w-full relative mb-10">
                 <p className="text-3xl my-5">{langPack.career[lang]}</p>
-                <p className="text-xl" style={{color: 'rgb(var(--gray-7))'}}>
-                  {langPack.careerdesc[lang]}
-                </p>
               </div>
       
               <div className="w-full">
